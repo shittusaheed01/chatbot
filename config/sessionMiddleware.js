25807e2f2f40a872a8c20d839ac5eda4232f5e9d
Saheed Shittu <shittusaheed01@gmail.com>
2023-03-16 09:22:11 +0100

    chnaged session middleware

--- config/sessionMiddleware.js
+++ config/sessionMiddleware.js
@@ -1,24 +1,28 @@
 const session = require("express-session");
-const MongoStore = require('connect-mongodb-session')(session);
+const MongoStore = require("connect-mongodb-session")(session);
 
 const { config } = require("./config");
 
 const store = new MongoStore({
 	uri: config.local_db,
 	databaseName: config.db_name,
-	collection: 'session',
-
+	collection: "session",
 });
-store.on('error', function(error) {
+store.on("error", function (error) {
 	console.log(error);
 });
 
 const sessionMiddleware = session({
 	secret: config.sessionSecret,
-	resave: false, 
+	resave: true,
 	saveUninitialized: true,
 	store: store,
-	cookie: { secure: false, maxAge: +config.sessionMaxAge },
+	cookie: {
+		secure: true,
+		maxAge: +config.sessionMaxAge,
+		domain: ".chatbot-zznn.onrender.com",
+		httpOnly: true,
+	},
 });
 
 module.exports = sessionMiddleware;
